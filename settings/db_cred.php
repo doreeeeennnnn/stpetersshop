0856924faa0ec7c740c4dd87106789a69c02409c
doreeeeennnnn <doreen.kpogoh@ashesi.edu.gh>
2024-04-12 20:10:10 +0000

    main changes

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,7 +1,12 @@
 <?php
 
-define ("DATABASE","petersshop");
-define ("SERVER","localhost");
-define ("USERNAME","root");
-define ("PASSWD","");
+/*
+mysql://bf16a5dc177ded:24de14e4@us-cluster-east-01.k8s.cleardb.net/heroku_0e76988615c460c?reconnect=true
+*/
 
+define ("DATABASE","heroku_0e76988615c460c");
+define ("SERVER","us-cluster-east-01.k8s.cleardb.net");
+define ("USERNAME","bf16a5dc177ded");
+define ("PASSWD","24de14e4");
+
+?>
--- stpetersapp/settings/db_cred.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define ("DATABASE","petersshop");
-define ("SERVER","localhost");
-define ("USERNAME","root");
-define ("PASSWD","");
-
-
-?>
